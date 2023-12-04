b8b5f3c50fe235c0738287650f0dc332b1712039
yashcrest <yashcrest@gmail.com>
2023-12-04 15:47:12 +1030

    adding dotenv package for env variable

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,32 @@
-const express = require('express');
+const express = require('express')
+const fetch = require('node-fetch');
+require('dotenv').config();
 const app = express();
 const port = 3000 || process.env.PORT
 
 
 //body parser middleware, since express now comes with body-parser middle by default we don't need to use the body-parser npm package.
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 //handling the post request from client
 app.post('/submit-form' , async (req, res) => {
-    
+    const recaptchaToken = req.body['g-recaptcha-response']; // you can view google documentation for this.
+    const secretKey = process.env.SECRET_KEY;
+
+
+    //verify the token with Google's recaptcha API
+    const googleVerifyUrl = `https://www.google.com/recaptcha/api/siteverify/?secret=${secretKey}&response=${recaptchaToken}`;
+    const response = await fetch(googleVerifyUrl, {method: 'POST'});
+    console.log(response);
+    const data = await response.text();
+
+    if(data.success) {
+        // still need to write the logic of saving the form data into DB.
+        res.send('Form received and recaptcha verified!')
+    } else {
+        res.send('recaptcha verification failed');
+    }
 })
 
 
